2dbc264fee3c4161b179ae9307ec0a3625bce5d0
Lucas Bueno <lucas-jbb@hotmail.com>
2020-05-28 19:47:37 -0300

    Finalizacao curso parte 2 aplicando promises, http e refatoracao para compatibilidade em outros browsers

--- Curso JavaScript avancado alura/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/controller/NegociacaoController.js
+++ Curso JavaScript avancado alura/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/controller/NegociacaoController.js
@@ -1,81 +1,133 @@
 class NegociacaoController {
+  constructor() {
+    this._ordemAtual = "";
 
-    constructor() {
+    let $ = document.querySelector.bind(document); //manter associacao com document
+    this._inputData = $("#data"); //manipulo o dom e tenho controle dos elementos apenas uma vez
+    this._inputQuantidade = $("#quantidade"); //id;
+    this._inputValor = $("#valor");
 
-        let $ = document.querySelector.bind(document); //manter associacao com document
-        this._inputData = $('#data'); //manipulo o dom e tenho controle dos elementos apenas uma vez
-        this._inputQuantidade = $('#quantidade') //id;
-        this._inputValor = $('#valor');
+    this._listaNegociacoes = new Bind(
+      new ListaNegociacoes(),
+      new NegociacoesView($("#negociacoesView")),
+      "adiciona",
+      "esvazia",
+      "ordena",
+      "inverteOrdem"
+    );
 
-        this._listaNegociacoes = new Bind(new ListaNegociacoes, new NegociacoesView($('#negociacoesView')), 'adiciona', 'esvazia');
+    this._mensagem = new Bind(
+      new Mensagem(),
+      new MensagemView($("#mensagemView")),
+      "texto"
+    );
+  }
 
-        this._mensagem = new Bind(new Mensagem(), new MensagemView($('#mensagemView')), 'texto');
+  adiciona(event) {
+    event.preventDefault(); //previnir submissao para capturar
+    try {
+      this._listaNegociacoes.adiciona(this._novaNegociao());
+      this._mensagem.texto = "Negociação adicionada com sucesso";
+      this._limparCamposEFocarCampoData();
+    } catch (erro) {
+      this._mensagem.texto = erro;
     }
+  }
 
-    adiciona(event) {
-        event.preventDefault(); //previnir submissao para capturar
-        this._listaNegociacoes.adiciona(this._novaNegociao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        this._limparCamposEFocarCampoData();
-    }
-
-    importaNegociacoes() {
-        let service = new NegociacoesService();
-
+  importaNegociacoes() {
+    /* antes com callback
         service.obterNegociacoesDaSemana((erro, negociacoes) => { //callback function, programacao assincrona de Error first
             if (erro) {
                 this._mensagem.texto = erro;
                 return;
             }
             negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso';
+
+            service.obterNegociacoesSemanaAnterior((erro, negociacoes) => {
+                if (erro) {
+                    this._mensagem.texto = erro;
+                    return;
+                }
+
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+
+                service.obterNegociacoesSemanaRetrasada((erro, negociacoes) => { //callback, sera executada após a função obterNegociacoes
+                    if (erro) {
+                        this._mensagem.texto = 'Não foi possível obter as negociações da semana retrasada';
+                        return;
+                    }
+                    negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações da semana retrasada importadas com sucesso';
+                });
+            });
+        }); 
+        */
+    //Promises
+    /*
+        service.obterNegociacoesDaSemana() //Faz uma promessa, e então
+            .then(negociacoes => { //tem o retorno da promise
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações da semana obtidas com sucesso';
+            })
+            .catch(erro => this._mensagem.texto = erro);
+
+        service.obterNegociacoesSemanaAnterior()
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações da semana anterior importada com sucesso';
+            })
+            .catch(erro => this._mensagem.texto = erro);
+
+        service.obterNegociacoesSemanaRetrasada()
+            .then(negociacoes => {
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações da semana retrasada importada com sucesso';
+            })
+            .catch(erro => this._mensagem.texto = erro);
+        */
+    let service = new NegociacoesService();
+    service
+      .obterNegociacoes()
+      .then((negociacoes) => {
+        negociacoes.forEach((negociacao) => {
+          this._listaNegociacoes.adiciona(negociacao);
+          this._mensagem.texto = "Negociaçoes obtidas com sucesso";
         });
+      })
+      .catch((erro) => {
+        this._mensagem.texto = erro;
+      });
+  }
+
+  _novaNegociao() {
+    return new Negociacao(
+      DateHelper.textoParaData(this._inputData.value),
+      this._inputQuantidade.value,
+      this._inputValor.value
+    );
+  }
+
+  apaga() {
+    this._listaNegociacoes.esvazia();
+    this._mensagem.texto = "Negociações apagadas com sucesso";
+    this._mensagemView.update(this._mensagem);
+  }
+
+  _limparCamposEFocarCampoData() {
+    this._inputData.value = "";
+    this._inputQuantidade.value = 0;
+    this._inputValor.value = 0.0;
+
+    this._inputData.focus();
+  }
+
+  ordena(coluna) {
+    if (this._ordemAtual == coluna) {
+      //Se X igual a X iverte a ordem
+      this._listaNegociacoes.inverteOrdem();
+    } else {
+      this._listaNegociacoes.ordena((a, b) => a[coluna] - b[coluna]);
     }
-
-    importaNegociacoesSemanaAnterior() {
-        let service = new NegociacoesService();
-
-        service.obterNegociacoesSemanaAnterior((erro, negociacoes) => {
-            if (erro) {
-                this._mensagem.texto = erro;
-                return;
-            }
-
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações da semana anterior importadas com sucesso';
-        })
-    }
-
-    importaNegociacoesSemanaRetrasada() {
-        let service = new NegociacoesService();
-
-        service.obterNegociacoesSemanaRetrasada((erro, negociacoes) => { //callback, sera executada após a função obterNegociacoes
-            if (erro) {
-                this._mensagem.texto = 'Não foi possível obter as negociações da semana retrasada';
-                return;
-            }
-            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações da semana retrasada importadas com sucesso';
-        });
-    }
-
-    _novaNegociao() {
-        return new Negociacao(DateHelper.textoParaData(this._inputData.value),
-            this._inputQuantidade.value,
-            this._inputValor.value);
-    }
-
-    apaga() {
-        this._listaNegociacoes.esvazia();
-        this._mensagem.texto = 'Negociações apagadas com sucesso';
-        this._mensagemView.update(this._mensagem);
-    }
-
-    _limparCamposEFocarCampoData() {
-        this._inputData.value = "";
-        this._inputQuantidade.value = 0;
-        this._inputValor.value = 0.0;
-
-        this._inputData.focus();
-    }
+    this._ordemAtual = coluna;
+  }
 }
--- Curso JavaScript avancado alura/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/services/NegociacoesService.js
+++ Curso JavaScript avancado alura/javascript-avancado-i-aula1/javascript-avancado-i-aula1/aluraframe/client/js/app/services/NegociacoesService.js
@@ -1,60 +1,84 @@
 class NegociacoesService {
+  constructor() {
+    this._http = new HttpService();
+  }
 
-    obterNegociacoesDaSemana(cb) { //callBack function
-        let xhr = new XMLHttpRequest();
+  obterNegociacoesDaSemana() {
+    return this._http
+      .get("/negociacoes/semana")
+      .then((negociacoes) => {
+        return negociacoes.map(
+          (objeto) =>
+            new Negociacao(
+              new Date(objeto.data),
+              objeto.quantidade,
+              objeto.valor
+            )
+        );
+      })
+      .catch((erro) => {
+        console.log(erro);
+        throw new Error("Não foi possível obter as negociações da semana");
+      });
+  }
 
-        xhr.open('GET', '/negociacoes/semana');
+  obterNegociacoesSemanaAnterior() {
+    return this._http
+      .get("/negociacoes/anterior")
+      .then((negociacoes) => {
+        return negociacoes.map(
+          (objeto) =>
+            new Negociacao(
+              new Date(objeto.data),
+              objeto.quantidade,
+              objeto.valor
+            )
+        );
+      })
+      .catch((erro) => {
+        console.log(erro);
+        throw new Error(
+          "Não foi possível obter as negociações da semana anterior"
+        );
+      });
+  }
 
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    cb(null, JSON.parse(xhr.responseText) //callBack
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))) //gera um novo array de negociacao
-                } else {
-                    console.log(xhr.responseText);
-                    cb('Não foi possível obter as negociações', null);
-                }
-            }
-        }
+  obterNegociacoesSemanaRetrasada() {
+    return this._http
+      .get("/negociacoes/retrasada")
+      .then((negociacoes) => {
+        return negociacoes.map(
+          (objeto) =>
+            new Negociacao(
+              new Date(objeto.data),
+              objeto.quantidade,
+              objeto.valor
+            )
+        );
+      })
+      .catch((erro) => {
+        console.log(erro);
+        throw new Error(
+          "Não foi possível obter as negociações da semana retrasada"
+        );
+      });
+  }
 
-        xhr.send();
-    }
-
-    obterNegociacoesSemanaAnterior(cb) {
-        let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', '/negociacoes/anterior');
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    cb(null, JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-                } else {
-                    console.log(xhr.responseText);
-                    cb('Não foi possível obter as negociações', null);
-                }
-            }
-        }
-        xhr.send();
-    }
-
-    obterNegociacoesSemanaRetrasada(cb) {
-        let xhr = new XMLHttpRequest();
-
-        xhr.open('GET', '/negociacoes/retrasada');
-
-        xhr.onreadystatechange = () => {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    cb(null, JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
-                } else {
-                    console.log(xhr.responseText);
-                    cb('Não foi possível obter as negociações da semana retrasada', null);
-                }
-            }
-        }
-        xhr.send();
-    }
+  obterNegociacoes() {
+    return Promise.all([
+      this.obterNegociacoesDaSemana(),
+      this.obterNegociacoesSemanaAnterior(),
+      this.obterNegociacoesSemanaRetrasada(),
+    ])
+      .then((periodos) => {
+        let negociacoes = periodos.reduce(
+          (dados, periodo) => dados.concat(periodo),
+          []
+        );
+        return negociacoes;
+      })
+      .catch((erro) => {
+        throw new Error(erro);
+      });
+  }
 }
